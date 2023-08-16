b357b9e7e13582d1e0ef24b0a36b8e80f6752337
Toby <tobycorreia@gmail.com>
2023-08-16 13:13:36 +0200

    Added rainbow color

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let gridSize = 32
+let gridSize = 20
 
 const grid = document.querySelector('.grid');
 const eraseButton = document.getElementById('erase');
@@ -24,15 +24,20 @@ grid.style.gridTemplateRows = `repeat(${gridSize}, calc(100% / ${gridSize}))`;
 const gridItems = document.querySelectorAll('.grid-item');
 let isDrawing = false;
 
+// Function to create a random color
+function randomColor() {
+    return "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
 gridItems.forEach(item => {
     item.addEventListener('mousedown', () => {
         isDrawing = true;
-        item.style.backgroundColor = '#3498db'; // Change color when clicked
+        item.style.backgroundColor = randomColor() // Change color when clicked
     });
 
     item.addEventListener('mouseover', () => {
         if (isDrawing) {
-            item.style.backgroundColor = '#3498db'; // Change color when dragging
+            item.style.backgroundColor = randomColor() // Change color when dragging
         }
     });
 
